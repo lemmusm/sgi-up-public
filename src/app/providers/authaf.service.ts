a7dea9d61d1dd33d77d3d4c756199c24ed83b6b3
lemmusm <lemmusm@gmail.com>
2020-01-14 21:48:36 -0600

    Update @angular/cli @angular/core, replace angularfire2 by @angular/fire, .gitignore

--- src/app/providers/authaf.service.ts
+++ src/app/providers/authaf.service.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 // import 'firebase/auth';
-import { Router } from '@angular/router';
+import { AngularFireAuth } from "@angular/fire/auth";
+import * as firebase from "firebase/app";
+import { Router } from "@angular/router";
 // sweetalert2
-import Swal from 'sweetalert2';
-import { AlertService } from './alert.service';
+import Swal from "sweetalert2";
+import { AlertService } from "./alert.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthafService {
   // variable login component
   login = false;
-  numeros = '0123456789';
+  numeros = "0123456789";
   email: string;
   // use info user from firebase
   // user: Observable<firebase.User>;
 
-  constructor(public afservice: AngularFireAuth, private router: Router, public alert: AlertService) {
+  constructor(
+    public afservice: AngularFireAuth,
+    private router: Router,
+    public alert: AlertService
+  ) {
     this.isAuth();
   }
 
@@ -34,7 +37,7 @@ export class AuthafService {
         // print status user
         // console.log(auth);
       } else {
-        this.router.navigate(['/login']);
+        this.router.navigate(["/login"]);
         this.login = false;
         // print status user
         // console.log(auth);
@@ -44,20 +47,25 @@ export class AuthafService {
   // Comprueba si la mátricula proviene de un alumno
   isAlumno() {
     // Obtiene el mail desde firebase y lo guarda en la variable, subtrae solo el usuario del correo
-    const user = this.email.substring(0, this.email.indexOf('@'));
+    const user = this.email.substring(0, this.email.indexOf("@"));
     switch (user) {
-      case '216030383':
-        this.alert.simpleAlert('¡AVISO!', 'ACCESO TEMPORAL', 'warning', '');
+      case "216030383":
+        this.alert.simpleAlert("¡AVISO!", "ACCESO TEMPORAL", "warning", "");
         break;
-      case '216030057':
-        this.alert.simpleAlert('¡AVISO!', 'ACCESO TEMPORAL', 'warning', '');
+      case "216030057":
+        this.alert.simpleAlert("¡AVISO!", "ACCESO TEMPORAL", "warning", "");
         break;
       default:
         for (let i = 0; i < user.length; i++) {
           if (this.numeros.indexOf(user.charAt(i), 0) !== -1) {
             this.signOut();
-            this.router.navigate(['/login']);
-            this.alert.simpleAlert('Acceso denegado', 'USUARIO NO AUTORIZADO', 'error', 'Reportar problema a mhernandez@uppenjamo.edu.mx');
+            this.router.navigate(["/login"]);
+            this.alert.simpleAlert(
+              "Acceso denegado",
+              "USUARIO NO AUTORIZADO",
+              "error",
+              "Reportar problema a mhernandez@uppenjamo.edu.mx"
+            );
             return 1;
           }
         }
@@ -66,35 +74,39 @@ export class AuthafService {
   }
 
   isAuth() {
-    return this.afservice.authState
-      .pipe(
-        map(fbUser => {
-          if (fbUser == null) {
-            this.router.navigate(['/login']);
-          }
-          return fbUser != null;
-        })
-      );
+    return this.afservice.authState.pipe(
+      map(fbUser => {
+        if (fbUser == null) {
+          this.router.navigate(["/login"]);
+        }
+        return fbUser != null;
+      })
+    );
   }
   // Método para iniciar sesión
   signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('email');
-    provider.setCustomParameters({ hd: 'uppenjamo.edu.mx' });
+    provider.addScope("email");
+    provider.setCustomParameters({ hd: "uppenjamo.edu.mx" });
     this.afservice.auth
       .signInWithPopup(provider)
       .then(data => {
-        this.router.navigate(['']);
+        this.router.navigate([""]);
         // this.alert.minimalAlert('Bienvenido a la Norma ISO 9001:2015');
       })
       .catch(error => {
-        this.alert.simpleAlert('Error: ', error.message, 'error', 'code: ' + error.code);
+        this.alert.simpleAlert(
+          "Error: ",
+          error.message,
+          "error",
+          "code: " + error.code
+        );
       });
   }
   // Método para cerrar sesión
   signOut() {
     this.afservice.auth.signOut().then(() => {
-      this.router.navigate(['/login']);
+      this.router.navigate(["/login"]);
     });
   }
 }
